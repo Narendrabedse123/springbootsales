ee17740a90d5eac633bdd7630d5f1d08df306268
James Ward <james@jamesward.org>
2016-07-18 20:36:24 -0600

    try new type for id

--- src/main/java/com/example/Bar.java
+++ src/main/java/com/example/Bar.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.UUID;
@@ -11,7 +12,7 @@ public class Bar {
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(columnDefinition = "BINARY(16)")
+    @Type(type = "pg-uuid")
     public UUID id;
 
     public String name;
