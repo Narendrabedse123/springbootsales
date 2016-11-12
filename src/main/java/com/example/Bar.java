a9ce566956d50d062e95e8af20388ac63d907b8e
James Ward <james@jamesward.org>
2016-11-12 16:23:10 -0700

    no more uuid

--- src/main/java/com/example/Bar.java
+++ src/main/java/com/example/Bar.java
@@ -1,19 +1,17 @@
 package com.example;
 
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
 
-import javax.persistence.*;
-import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Bar {
 
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Type(type = "pg-uuid")
-    public UUID id;
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    public Long id;
 
     public String name;
 
